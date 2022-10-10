248be3d58443a0f8b7bbbfdfc556d3bbc9a7a1f4
Aidarbek Umurzakov <aumurzakov@it.gov.kg>
2022-10-10 15:06:50 +0600

    margin-mistakes

--- pages/burgers/index.js
+++ pages/burgers/index.js
@@ -1,4 +1,18 @@
-const Burgers = () => {
+
+export const getStaticProps = async () => {
+    const res = await fetch('http://localhost:4000/items');
+    const data =  await res.json();
+    
+    return{
+        props:{
+            burgers: data
+        }
+    }
+}
+
+
+const Burgers = (props) => {
+    console.log('props >>>> ', props);
     return (
         <div>
             <h1>Our Burgers</h1>
